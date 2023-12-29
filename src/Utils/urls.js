58abaf8dc2cb6e63caa4506b85627419f53d4aa3
Ahammed <ahammedrihancm@gmail.com>
2023-12-29 10:21:25 +0530

    connected with backend

--- src/Utils/urls.js
+++ src/Utils/urls.js
@@ -1,6 +1,6 @@
-// export const baseUrl = "https://backend.eco-ride.online/";
+export const baseUrl = "https://backend.eco-ride.online/";
 
-export const baseUrl = "http://127.0.0.1:8000/";
+// export const baseUrl = "http://127.0.0.1:8000/";
 
 
 
